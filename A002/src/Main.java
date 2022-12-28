c91bbbe3c353b1f0245b35218ce12b593fae55c7
jang-yujin <jyj9747@naver.com>
2022-12-28 15:44:20 +0900

    A002

--- A002/src/Main.java
+++ A002/src/Main.java
@@ -3,12 +3,13 @@ import java.util.*;
 * Pascal's triangle
 * https://leetcode.com/problems/pascals-triangle/
 *
+* 파스칼 트라이앵글을 이중 리스트로 만들어 반환한다.
 * */
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<List<Integer>> list = s.generate(5);
-        System.out.println(list);가
+        System.out.println(list);
     }
 }
