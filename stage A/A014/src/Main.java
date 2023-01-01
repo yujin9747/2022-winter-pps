48fc523efae27569cdbed43e2569e2647ee8d41d
jang-yujin <jyj9747@naver.com>
2023-01-01 17:43:16 +0900

    A014

--- stage A/A014/src/Main.java
+++ stage A/A014/src/Main.java
@@ -1,10 +1,15 @@
 import java.util.List;
-
+/**
+ * https://leetcode.com/problems/summary-ranges/
+ * Summary ranges
+ *
+ * 정렬된 문자 배열안에서 range 정리하점
+ * **/
 public class Main {
     public static void main(String[] args) {
 
         Solution s = new Solution();
-        int[] nums = {0,1,2,4,5,7};
+        int[] nums = {-1};
         List<String> result = s.summaryRanges(nums);
         for(int i=0; i<result.size(); i++) System.out.println(result.get(i));
     }
--- stage A/A014/src/Solution.java
+++ stage A/A014/src/Solution.java
@@ -1,7 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<String>();
 
+        // 숫자가 없는 경우
+        if(nums.length == 0) return result;
+        // 숫자가 하나인 경우
+        else if(nums.length == 1) {
+            result.add(nums[0] + "");
+        }
+
+        int cur = nums[0];
+        int expected = nums[0] + 1;
+
+        for(int i=1; i<nums.length; i++){
+            // 마지막 숫자인 경우
+            if(i == nums.length-1) {
+                // cur -> 마지막 으로 범위가 끝나는 경우
+                if(expected == nums[i]) result.add(cur + "->" + nums[i]);
+                else {
+                    // cur->cur 범위가 만들어지는 경우
+                    if(expected-1 == cur) result.add(cur + "");
+                    // cur->expected-1 범위가 만들어지는 경우
+                    else result.add(cur + "->" + (expected-1));
+
+                    // 마지막 숫자 -> 마지막 숫자 범위
+                    result.add(nums[i] + "");
+                }
+            }
+            // 마지막 숫자 X, 범위가 이어지는 경우
+            else if(nums[i] == expected){
+                expected++;
+            }
+            // 범위가 끊기는 경우
+            else {
+                // cur->cur 범위가 만들어지는 경우
+                if(expected-1 == cur) result.add(cur + "");
+                // cur->expected-1 범위가 만들어지는 경우
+                else result.add(cur + "->" + (expected-1));
+
+                // cur : 새로운 범위 시작지점
+                cur = nums[i];
+                expected = nums[i] + 1;
+            }
+        }
+        return result;
     }
 }
