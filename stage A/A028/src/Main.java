fc32a590525a7c8e1b3d38ced331811485518992
jang-yujin <jyj9747@naver.com>
2023-01-04 21:18:20 +0900

    A028

---
+++ stage A/A028/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * 큰 수 더하기
+ * https://www.acmicpc.net/problem/10757
+ * Double 사용
+ * 와이파이 때문에 제출해보지는 못함
+ * **/
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
---
+++ stage A/A028/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public void solution(){
+        Scanner s = new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        Double sum = a + b;
+        System.out.printf("%f", sum);
+    }
+}
